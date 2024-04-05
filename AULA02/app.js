3e8437495cca3549007f96e6ef6fd84838ed58c0
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-05 11:55:42 -0300

    adicionando leitura síncrona

--- AULA02/app.js
+++ AULA02/app.js
@@ -1,7 +1,11 @@
 //ler arquivos com o node
 //lib fs(file System)
 const fs = require("fs");
-fs.readFile("AULA02/dados.csv", (erro, data) => {
-  //? Optei pela utilização do operador ternário
-  erro ? console.log(erro) : console.log(data.toString());
-});
+// //! Método Assíncrono
+// fs.readFile("AULA02/dados.csv", (erro, data) => {
+//   //? Optei pela utilização do operador ternário
+//   erro ? console.log(erro) : console.log(data.toString());
+// });
+let dados = fs.readFileSync("AULA02/dados.csv");
+console.log(dados.toString());
+console.log("Segunda operação");
