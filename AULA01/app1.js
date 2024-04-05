97d198c8ee5ca31be88e26babec62bdf5c24dcf8
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-05 09:00:01 -0300

    modificando a entrada dos dados

--- AULA01/app1.js
+++ AULA01/app1.js
@@ -1,6 +1,6 @@
-const pessoas = require("./dados");
+const { nomes, frutas, soma } = require("./dados");
 // console.log(pessoas);
 // console.log(pessoas[0]);
-console.log(pessoas.nomes);
-console.log(pessoas.frutas);
-console.log(pessoas.soma(2, 3));
+console.log(nomes);
+console.log(frutas);
+console.log(soma(2, 3));
