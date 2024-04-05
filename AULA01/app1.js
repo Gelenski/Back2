977897e070966531c9fb283eadf0388859d3a08c
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-05 08:44:24 -0300

    adicionando e importando os dados

---
+++ AULA01/app1.js
@@ -0,0 +1,3 @@
+const pessoas = require("./dados");
+console.log(pessoas);
+console.log(pessoas[0]);
---
+++ AULA01/dados.js
@@ -0,0 +1,7 @@
+//Criação de um array de nomes.
+let nomes = ["Admilson", "Berenilda", "Ceciliano"];
+let frutas = ["amora", "banana", "caqui"];
+// module.exports = nomes;
+// module.exports = frutas;
+module.exports = { nomes, frutas };
+''
