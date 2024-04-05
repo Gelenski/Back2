b785201a9a037ca1a193fb9db921f1576a590e18
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-05 12:34:26 -0300

    Tratando com exclusões

---
+++ AULA03/app.js
@@ -0,0 +1,13 @@
+//Criação de pastas
+const fs = require("fs");
+// fs.mkdirSync("/logs");
+// fs.mkdir("logs2", (erro) => {
+//   if (erro) {
+//     console.log("Erro");
+//   }
+// });
+
+if (fs.existsSync("logs2")) {
+  fs.rmdirSync("logs2");
+}
+console.log("Fim da execução");
