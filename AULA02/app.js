959a6219cad5b53b8ec7e2caf6e06d38461aa949
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-05 12:00:01 -0300

    adicionando arquivo de configuração do prettier e comentando o código

--- AULA02/app.js
+++ AULA02/app.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 // fs.readFile("AULA02/dados.csv", (erro, data) => {
 //   //? Optei pela utilização do operador ternário
 //   erro ? console.log(erro) : console.log(data.toString());
+//   console.log("Segunda operação")
 // });
 let dados = fs.readFileSync("AULA02/dados.csv");
 console.log(dados.toString());
