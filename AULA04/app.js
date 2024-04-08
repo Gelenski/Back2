cd68c14e457d17d37d38b1b6d0072e7519db6d2d
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-08 08:16:37 -0300

    log

--- AULA04/app.js
+++ AULA04/app.js
@@ -1,6 +1,6 @@
 //Como criar arquivos
 const fs = require("fs");
-let log = "mensagem de log\n";
+let log = "Mensagem de log\n";
 //Criação de arquivo de log síncrono
 fs.writeFileSync("./logs.log", log);
 console.log("Arquivo criado com sucesso");
