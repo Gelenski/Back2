8473aa6e32bbee11d98b80b29817924f011e1d15
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-08 08:37:26 -0300

    Lidando com exclusões de arquivos

--- AULA04/app.js
+++ AULA04/app.js
@@ -4,4 +4,16 @@ let log = "Mensagem de log\n";
 //Criação de arquivo de log síncrono
 fs.writeFileSync("./logs.log", log, { flag: "a+" });
 
+//Criação de arquivo de forma assíncrona
+fs.writeFile("./logs2.log", log, { flag: "a+" }, (erro) => {
+  if (erro) {
+    console.log(erro);
+  }
+});
+
 console.log("Arquivo criado com sucesso");
+
+//Exclusão de arquivos
+// fs.rmSync("./logs.log");
+fs.unlinkSync("./logs.log");
+console.log("Arquivo removido");
